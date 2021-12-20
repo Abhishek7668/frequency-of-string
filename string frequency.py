0df3a879743ff79b7a786aa24c4cdea2a4b541e6
Abhishek7668 <92740857+Abhishek7668@users.noreply.github.com>
2021-12-20 22:41:07 +0530

    Add files via upload

---
+++ string frequency.py
@@ -0,0 +1,17 @@
+st = input("enter any string")
+ch = input("enter any charcter")
+c = 0
+for i in st:
+     if ch==i:
+        out = i.count(ch)
+        print(out)
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
